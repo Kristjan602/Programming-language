e87ea561f79562ef3c8893358c846aeaa2292a2a
Kristjan602 <kristjan602@gmail.com>
2025-05-26 16:24:01 +0300

    Update shell.py

--- shell.py
+++ shell.py
@@ -2,11 +2,6 @@ import main
 
 while True:
     text = input("Main > ")
-    if text == "run code":
-        with open('code.txt', 'r', encoding='utf-8') as f:
-            text = f.read()
-        text = text.replace('\n', '?')
-
     result, error = main.run('<stdin>', text)
     if error:
         print(error.as_string())
